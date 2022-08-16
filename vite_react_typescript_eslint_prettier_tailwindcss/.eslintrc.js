bb5394fc4a3d785c6856e09c30f21bce562731be
BarB93 <barovskiyboris@gmail.com>
2022-08-16 20:28:27 +0300

    added boilerplates for vite-react-ts and vite-react-ts-tailwind

---
+++ vite_react_typescript_eslint_prettier_tailwindcss/.eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended', // Make sure this is always the last element in the array.
+  ],
+  plugins: ['simple-import-sort', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-console': 'warn',
+    semi: ['error', 'never'],
+    'no-debugger': 'warn',
+    'prefer-const': 'warn',
+    yoda: 'warn',
+    'no-var': 'warn',
+    'no-undefined': 'warn',
+    'jsx-quotes': ['error', 'prefer-single'],
+  },
+}
